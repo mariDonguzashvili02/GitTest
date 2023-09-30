c1c757ee5750b6fde816f9a3b197f6c1a46db0a4
S <s>
2023-09-30 12:54:23 +0400

    {"types":"C","runs":18}

--- Problem05.java
+++ Problem05.java
@@ -1,4 +1,21 @@
-
-public class Problem05 {
+import stanford.karel.Karel;
+//5. კარელი დგას 1x1 უჯრაზე, შეავსებინეთ მას პირველი ქუჩა ბრილიანტებით. ანუ პირველი
+//ქუჩის ყველა უჯრაზე უნდა იდოს ზუსტად ერთი ბრილიანტი. ჩათვალეთ რომ საწყისი
+//სამყარო ცარიელი არ არის და გარკვეულ(ჩვენთვის უცნობ) უჯრებში თითო(მხოლოდ ერთი)
+//ბრილიანტი დევს.
+public class Problem05 extends Karel{
+	public void run(){
+		while(frontIsClear(){
+			
+			if(noBeepersPresent()){
+				putBeeper();
+				move();
+				if(frontIsBlocked()){
+					putBeeper();
+				}
+			}
+			
+		}
+	}
 
 }
