d8a00fb0e0da16f0129c5bcb01fe9f0e5d7da8de
S <s>
2023-10-10 19:55:18 +0400

    {"types":"C","runs":11}

--- Problem10.java
+++ Problem10.java
@@ -14,6 +14,7 @@ public class Problem10 extends Karel {
 			while (beepersPresent()) {
 				puttingBackDevisor();
 			}
+			putBeeper();
 		}
 
 	}
@@ -65,6 +66,7 @@ public class Problem10 extends Karel {
 
 		pickBeeper();
 		turnAround();
+		move();
 		putBeeper();
 		turnAround();
 		move();
