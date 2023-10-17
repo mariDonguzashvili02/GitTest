0e1f069509ab565fa1d621fb668873393cb0e13e
S <s>
2023-10-17 18:37:25 +0400

    {"types":"C","runs":4}

--- Problem17.java
+++ Problem17.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
+//17. კონსოლიდან წაიკითხეთ ნამდვილი რიცხვი და დაბეჭდეთ მისი მთელი ნაწილი და მისი
+//წილადი ნაწილი. მაგალითად მომხმარებელმა თუ შეიყვანა 3.14 თქვენმა პროგრამამ უნდა
+//დაბეჭდოს 3 და 0.14
+public class Problem17 extends ConsoleProgram{
 
-public class Problem17 {
-
+	public void run(){
+		
+		double number = readDouble ("Enter the number: ");
+		int wholeNum = (int)number;
+		double substraction = number - wholeNum;
+		println(substraction);
+		
+	}
 }
