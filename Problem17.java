9429db97447675cf801bd67dcdc45180f2847bd8
S <s>
2023-10-17 18:56:02 +0400

    {"types":"C","runs":7}

--- Problem17.java
+++ Problem17.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 import acm.program.ConsoleProgram;
 //17. კონსოლიდან წაიკითხეთ ნამდვილი რიცხვი და დაბეჭდეთ მისი მთელი ნაწილი და მისი
 //წილადი ნაწილი. მაგალითად მომხმარებელმა თუ შეიყვანა 3.14 თქვენმა პროგრამამ უნდა
@@ -6,10 +8,22 @@ public class Problem17 extends ConsoleProgram{
 
 	public void run(){
 		
-		double number = readDouble ("Enter the number: ");
-		int wholeNum = (int)number;
-		double substraction = number - wholeNum;
-		println(substraction);
+//		double number = readDouble ("Enter the number: ");
+//		int wholeNum = (int)number;
+//		double substraction = number - wholeNum;
+//		println("Integer part" + wholeNum);
+//		println("Fraction part"+ substraction);
+		calculateInBigDecimal();
+		
+	}
+	private void calculateInBigDecimal(){
+		double d = readDouble("Enter number: ");
+		String dAsAString = "" + d;
+		BigDecimal decimal = new BigDecimal(d);
+		println(decimal);
+		
+		int integerPart = decimal.intValue();
+		BigDecimal fractionPart = decimal.remainder(BigDecimal.ONE);
 		
 	}
 }
