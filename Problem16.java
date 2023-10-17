5645bb5386ba109a36a18708d50aa6aaa7d25289
S <s>
2023-10-17 18:26:57 +0400

    {"types":"C","runs":1}

--- Problem16.java
+++ Problem16.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
+//16. კონსოლიდან წაიკითხეთ სამი მთელი რიცხვი და დაბეჭდეთ მათი საშუალო ჰარმონიული.
+//შეგახსენებთ, რომ სამი რიცხვის საშუალო ჰარმონიულის გამოთვლის ფორმულა არის (1/(1/a + 1/b +1/c))
+public class Problem16 extends ConsoleProgram{
 
-public class Problem16 {
-
+	public void run(){
+		
+		int a = readInt("Enter the first number");
+		int b = readInt ("Enter the second number");
+		int c = readInt ("Enter the third number");
+		
+		int harm = 1/(1/a + 1/b + 1/c);
+		println(harm);
+	}
 }
