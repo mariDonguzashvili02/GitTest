b804230775db0596b933b489399829b9485554a3
S <s>
2023-10-21 12:24:18 +0400

    {"types":"C","runs":9}

--- Problem20.java
+++ Problem20.java
@@ -1,4 +1,8 @@
+import acm.program.ConsoleProgram;
+//20. კონსოლიდან წაიკითხეთ n, შემდეგ წაიკითხეთ n ცალი მთელი რიცხვი და დაბეჭდეთ ჯამი
+public class Problem20 extends ConsoleProgram{
 
-public class Problem20 {
-
+	public void run(){
+		
+	}
 }
