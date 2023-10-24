7320c0d596313f74142cee66901acf1b860ba9c7
S <s>
2023-10-24 18:21:26 +0400

    {"types":"C","runs":14}

--- Problem21.java
+++ Problem21.java
@@ -2,26 +2,25 @@
 import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
-public class Problem21 extends GraphicsProgram{
-	public void run(){
-		 
+public class Problem21 extends GraphicsProgram {
+	public void run() {
+
 		double x1 = 0;
 		double y1 = 10;
 		double x2 = 100;
 		double y2 = 100;
-		
-		for(int i = 0; i > 10 ; i++){
-			
+
+		for (int i = 0; i < 10; i++) {
+
 			x1 += 10;
 			x2 += 10;
 			y2 += 10;
-			
-			GLine verticalLine = new GLine(x1,y1,x1,y2);
+
+			GLine verticalLine = new GLine(x1, y1, x1, y2);
 			add(verticalLine);
-		
+
 		}
-		
-		
+
 	}
 
 }
