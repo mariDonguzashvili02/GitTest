60f1973f58ecbb07f2e622d3dea7d0beae03fd9e
S <s>
2023-10-24 18:21:27 +0400

    {"types":"C","runs":14}

--- Problem23_24.java
+++ Problem23_24.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Problem23_24 {
+//23. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს 1 დან n მდე რიცხვების
+//ჯამი
+//24. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს n ის გამყოფების
+//რაოდენობა
+public class Problem23_24 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Enter n: ");
+		int sum = calculateSum(n);
+		println("Sum is :" + sum);
+	}
 
+	private int calculateSum(int n) {
+		for(int i = 0; i < n, i++){
+			
+		}
+		return 0;
+	}
 }
