aa816acc035e39f7e6c1f4c4927a1978033c0121
S <s>
2023-10-27 16:12:15 +0400

    {"types":"C","runs":12}

--- Problem28.java
+++ Problem28.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Problem28 {
+//28. მომხმარებელს შეყავს დადებითი რიცხვები, მანამ სანამ არ შეიყვანს -1 ს, დაბეჭდეთ
+//რაოდენობა რამდენი ლუწი რიცხვი შეყვანა მომხმარებელმა.
+public class Problem28 extends ConsoleProgram {
+	public void run() {
+
+		int n = readInt("Enter the number: ");
+		int sum = 0;
+		for(int i = 0; i < n + 1; i++){
+			int m = readInt("Enter the number again until you reach -1:");
+			if(m % 2 == 0){
+				sum += m ;
+			}
+			
+		}
+		println(sum);
+	}
 
 }
