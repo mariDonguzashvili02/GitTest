b77d3edeb3264e64a554a332ca3886c13b46e87a
S <s>
2023-10-27 13:15:55 +0400

    {"types":"C","runs":3}

--- Problem21.java
+++ Problem21.java
@@ -10,7 +10,7 @@ public class Problem21 extends GraphicsProgram {
 		double x2 = 100;
 		double y2 = 110;
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 11; i++) {
 			
 			GLine verticalLine = new GLine(x1, y1, x1, y2);
 			add(verticalLine);
@@ -21,7 +21,7 @@ public class Problem21 extends GraphicsProgram {
 		y1 = 10;
 		y2 = 100;
 				
-		for(int i = 0; i < 10; i++){
+		for(int i = 0; i < 11; i++){
 			
 			GLine horizontalLine = new GLine(x1, y1, x2, y1);
 			add(horizontalLine);
