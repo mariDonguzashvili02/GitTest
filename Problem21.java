bc17fb228ebc9f93dbb86f8bc8bb774f5b328605
S <s>
2023-10-27 13:14:37 +0400

    {"types":"C","runs":1}

--- Problem21.java
+++ Problem21.java
@@ -5,12 +5,12 @@ import acm.program.GraphicsProgram;
 public class Problem21 extends GraphicsProgram {
 	public void run() {
 
-		double x1 = 0;
+		double x1 = 10;
 		double y1 = 10;
 		double x2 = 100;
 		double y2 = 110;
 
-		for (int i = 0; i < 11; i++) {
+		for (int i = 0; i < 10; i++) {
 
 			x1 += 10;
 		
@@ -21,10 +21,10 @@ public class Problem21 extends GraphicsProgram {
 		}
 		x1 = 10;
 		x2 = 110;
-		y1 = 0;
+		y1 = 10;
 		y2 = 100;
 				
-		for(int i = 0; i < 11; i++){
+		for(int i = 0; i < 10; i++){
 			y1 += 10;
 			GLine horizontalLine = new GLine(x1, y1, x2, y1);
 			add(horizontalLine);
