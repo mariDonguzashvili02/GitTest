7769146d676fd97569179e83d22d5fd522474c97
S <s>
2023-11-02 18:21:16 +0400

    {"types":"C","runs":56}

--- Problem34.java
+++ Problem34.java
@@ -1,4 +1,14 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Problem34 {
+public class Problem34 extends GraphicsProgram {
+	public void run(){
+		RandomGenerator rgen = RandomGenerator.getInstance();
+		Math.max(3,5);
+		GRect myRect = new GRect(10,20);
+		myRect.setFilled(true);
+		int a = rgen.nextInt(6);
+	}
 
 }
