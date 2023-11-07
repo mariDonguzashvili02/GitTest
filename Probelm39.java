0d674278ebbc88b41a6622baa7d516d778c1a906
S <s>
2023-11-07 18:58:00 +0400

    {"types":"C","runs":13}

--- Probelm39.java
+++ Probelm39.java
@@ -1,4 +1,29 @@
+import java.awt.Color;
 
-public class Probelm39 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+//39. დაწერეთ პროგრამა სადაც იქნება კანვასზე ბურთის მოძრაობა მარცხენა კედლის შუა
+//წერტილიდან მარჯვენა კედლამდე.
+public class Probelm39 extends GraphicsProgram {
+	private static final int radius = 50;
+	private static final int PAUSE_MS = 20;
+	private static final int VEL_X = 3;
+	public void run(){
+		
+		
+		GOval circle = new GOval(radius, radius);
+		circle.setFilled(true);
+		circle.setColor(Color.RED);
+		double x = 0;
+		double y = getWidth()/ 2;
+		add(circle, x, y);
+		double finalX = getWidth();
+		
+		while(circle.getX() <= finalX){
+			circle.move(VEL_X, 0);
+			pause(PAUSE_MS);
+		}
+		
+	}
 
 }
