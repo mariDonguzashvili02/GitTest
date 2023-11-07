da90b7dbad4320cbf9da8b4e25efec5f03940c95
S <s>
2023-11-07 18:36:20 +0400

    {"types":"C","runs":4}

--- GraphicsSample2.java
+++ GraphicsSample2.java
@@ -1,4 +1,19 @@
+import java.awt.Color;
 
-public class GraphicsSample2 {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class GraphicsSample2 extends GraphicsProgram{
+	private static final int PAUSE_MS = 1000;
+	public void run(){
+		
+		GRect myRect = new GRect(100,200);
+		myRect.setFilled(true);
+		add(myRect, 100, 150);
+		
+		myRect.setColor(Color.RED);
+		pause(PAUSE_MS);
+		myRect.setColor(Color.BLUE);
+	}
 
 }
