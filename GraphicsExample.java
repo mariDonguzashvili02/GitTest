4f333564d01f22a0a0b400182fb06ed67996c518
S <s>
2023-11-09 18:52:29 +0400

    {"types":"C","runs":1}

--- GraphicsExample.java
+++ GraphicsExample.java
@@ -1,4 +1,18 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class GraphicsExample {
-
+public class GraphicsExample  extends GraphicsProgram{
+	public void init(){
+		
+	}
+	public void run(){
+		GRect rect = new GRect(100,100);
+		
+		
+		addMouseListeners();
+		
+		while(true){
+			pause(10);
+		}
+	}
 }
