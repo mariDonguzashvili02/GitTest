12814570f8691cae58ec81ce2686bb43ed08adf6
S <s>
2023-12-01 14:26:00 +0400

    {"types":"C","runs":26}

--- Problem48.java
+++ Problem48.java
@@ -3,34 +3,36 @@ import acm.program.ConsoleProgram;
 //48. კონსოლიდან შეგყვავს ტექსტი პროგრამამ უნდა დაბეჭდოს სტატისტიკა რომელი სიმბოლო
 //რამდენჯერ გვხვდება
 
-public class Problem48 extends ConsoleProgram{
-	public void run(){
+public class Problem48 extends ConsoleProgram {
+	public void run() {
 		String str = "karada";
 		symbolStatistics(str);
 	}
-	private void symbolStatistics(String text){
-		for(int i = 0; i < text.length(); i++){
+
+	private void symbolStatistics(String text) {
+
+		for (int i = 0; i < text.length(); i++) {
 			char c = text.charAt(i);
-			if(isFirstOcurrence(text, i)){
+			if (isFirstOcurrence(text, i)) {
 				println("Symbol " + c + " occurs " + symbolCounter(text, c) + " times");
 			}
 		}
 	}
-	
-	private int symbolCounter(String text, char c){
-		int count = 0; 
-		for(int i = 0; i < text.length(); i++){
-			if(text.charAt(i) == c){
+
+	private int symbolCounter(String text, char c) {
+		int count = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == c) {
 				count++;
 			}
 		}
 		return count;
 	}
-	
-	private boolean isFirstOcurrence(String text, int index){
+
+	private boolean isFirstOcurrence(String text, int index) {
 		char c = text.charAt(index);
 		int firstIndex = text.indexOf(c);
-		
+
 		return firstIndex == c;
 	}
 }
