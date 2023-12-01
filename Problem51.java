be100257b365de31276170b32ffe7555d88be0ad
S <s>
2023-12-01 16:10:41 +0400

    {"types":"C","runs":39}

--- Problem51.java
+++ Problem51.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem51 {
+//51. დაწერეთ მეთოდი რომელიც გადაცემული ტექსტიდან შლის დუბლირებულ სიმბოლოებს,
+//ანუ თუ ტექსტიში რომელიმე სიმბოლო მეორდება, უნდა დარჩეს მხოლოდ ყველაზე მარცხენა.
 
+public class Problem51 extends ConsoleProgram{
+	public void run(){
+		String text = readLine("Enter text: ");
+		println(removeDuplicates(text));
+	}	
+	private String removeDuplicates(String text){
+		String result = "";
+		for(int i = 0; i < text.length(); i++){
+			char c = text.charAt(i);
+			if(result.charAt(i) == -1){
+				result += c;
+			}
+		}
+		return result;
+	}
 }
