39170a83bfeba9e6bbdd529280126a3da7362a17
S <s>
2023-12-01 12:09:48 +0400

    {"types":"C","runs":5}

--- Problem46.java
+++ Problem46.java
@@ -1,4 +1,37 @@
+import acm.program.ConsoleProgram;
 
-public class Problem46 {
+//46. დაწერეთ მეთოდი რომელსაც გადაეცემა სტიგნი და რომელიც აბრუნებს ამ სტრინგში
+//ყველაზე ხშირ სიმბოლოს.
+
+public class Problem46 extends ConsoleProgram{
+  public void run(){
+	
+	String str = "hello";
+
+	println(symbolComparer(str));
+  }
+
+  private char symbolComparer(String text){
+	int maxCharNum = 0;
+	char frequentChar = 0;
+	for(int i = 0; i < text.length(); i++){
+		int currChar = symbolCounter(text, text.charAt(i));
+		if(currChar > maxCharNum){
+			maxCharNum = currChar;
+			frequentChar = text.charAt(i);
+		}
+	}
+	return frequentChar;
+  }
+ 
+  private int symbolCounter(String text, char c){
+	int count = 0;
+	for(int i = 0; i < text.length(); i++){
+		if(text.charAt(i) == c){
+			count++;
+		}
+	}
+	return count;
+  }
 
 }
