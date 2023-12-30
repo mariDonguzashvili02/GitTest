d7f64473f723d48d70e9459f6477a73a36af2f2d
S <s>
2023-12-31 01:54:11 +0400

    {"types":"C","runs":28}

--- nothing.java
+++ nothing.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class nothing extends ConsoleProgram {
 	public void run(){
-		int[] dice = {1, 2, 3, 3, 3};
+		int[] dice = {1, 2, 3, 4, 5};
 		if(checkForFullHouse(dice)){
 			println("true");
 		}else{
