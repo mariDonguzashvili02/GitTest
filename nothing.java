5636046c80630ac4bb9268587fe6a9f258766461
S <s>
2023-12-31 01:46:25 +0400

    {"types":"C","runs":20}

--- nothing.java
+++ nothing.java
@@ -2,28 +2,53 @@ import acm.program.ConsoleProgram;
 
 public class nothing extends ConsoleProgram {
 	public void run(){
-		int[] dice = {5, 1, 3, 4, 6};
-		if(checkForStreets(dice)){
+		int[] dice = {5, 5, 3, 3, 3};
+		if(checkForFullHouse(dice)){
 			println("true");
 		}else{
 			println("false");
 		}
 	}
 	
-	private boolean checkForStreets(int[] dice){
-		int[] template = {1, 2, 3, 4, 5, 6};
-		int count = 0;
-		for(int i = 0; i < dice.length; i++){
-			for(int j = 0; j < dice.length; j++){
-				if(dice[j] == template[i]){
+	private boolean checkForFullHouse(int[] dice) {
+
+		for (int i = 0; i < dice.length; i++) {
+			int element = dice[i];
+			int count = 1;
+			for (int j = 0; j < dice.length; j++) {
+				if (dice[j] == element) {
 					count++;
-					break;
+					if (count == 3) {
+
+						int[] twos = elementRemover(dice, dice[j]);
+						if (twos[0] == twos[1]) {
+							return true;
+						}
+					}
 				}
 			}
 		}
-		if(count == 4 || count == 5){
-			return true;
-		}
+		
 		return false;
+	}
+	
+	private int[] elementRemover(int[] arr, int element) {
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == element) {
+				count++;
+			}
+		}
+		int n = 0;
+		int[] newArray = new int[arr.length - count];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != element) {
+				newArray[n] = arr[i];
+				n++;
+			}
+		}
+		return newArray;
 	}
 }
