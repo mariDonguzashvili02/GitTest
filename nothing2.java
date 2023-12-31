de05df6ea83d556f3d2f42d77ccd9f917c34cf69
S <s>
2023-12-31 04:50:37 +0400

    {"types":"C","runs":81}

--- nothing2.java
+++ nothing2.java
@@ -4,7 +4,7 @@ import acm.program.ConsoleProgram;
 
 public class nothing2 extends ConsoleProgram {
 	public void run(){
-		int[] dice = {2, 3, 4, 5, 2};
+		int[] dice = {2, 3, 6, 5, 1};
 		if(checkForSmallStraight(dice)){
 			println("True");
 		}else{
