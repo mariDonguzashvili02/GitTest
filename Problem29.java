086cf758fab3c27d41f27783eca0358c7b067578
S <s>
2024-01-06 17:46:01 +0400

    {"types":"C","runs":9}

--- Problem29.java
+++ Problem29.java
@@ -3,29 +3,31 @@ import acm.program.ConsoleProgram;
 //29. მოხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს ფიბონაჩის მიმდევრობის
 //მე n წევრი.
 public class Problem29 extends ConsoleProgram {
-	public void run(){
-		int n = readInt("Please, enter Fibonacci numbers bound: ");
-		
-		int fib = fibonacci(n);
-		println(fib);
-		
+	public void run() {
+		while (true) {
+			int n = readInt("Please, enter Fibonacci numbers bound: ");
+
+			int fib = fibonacci(n);
+			println(fib);
+		}
+
 	}
-	
-	private int fibonacci(int n){
-		if(n == 0 || n == 1){
+
+	private int fibonacci(int n) {
+		if (n == 0 || n == 1) {
 			return n;
 		}
-		
+
 		int f = -1;
 		int f1 = 1;
 		int f2 = 0;
-		
-		while(n > 0){
+
+		while (n > 0) {
 			f = f1 + f2;
 			f2 = f1;
 			f1 = f;
 		}
-		
+
 		return f;
 	}
 }
