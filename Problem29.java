f6612791f1f24c1cbe492b7d6345efeccc113f9e
S <s>
2024-01-06 16:59:21 +0400

    {"types":"C","runs":4}

--- Problem29.java
+++ Problem29.java
@@ -6,8 +6,8 @@ public class Problem29 extends ConsoleProgram {
 	public void run(){
 		int n = readInt("Please, enter Fibonacci numbers bound: ");
 		int f = -1;
-		int f1 = 1;
-		int f2 = 0;
+		int f1 = 0;
+		int f2 = 1;
 		
 		while(n > 0){
 			f = f1 + f2;
