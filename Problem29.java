32009dd85aebe923a6dea54e3d05305b365ac808
S <s>
2024-01-06 17:44:52 +0400

    {"types":"C","runs":7}

--- Problem29.java
+++ Problem29.java
@@ -4,42 +4,28 @@ import acm.program.ConsoleProgram;
 //მე n წევრი.
 public class Problem29 extends ConsoleProgram {
 	public void run(){
-//		int n = readInt("Please, enter Fibonacci numbers bound: ");
-//		int f = -1;
-//		int f1 = 1;
-//		int f2 = 0;
-//		
-//		while(n > 0){
-//			f = f1 + f2;
-//			f2 = f1;
-//			f1 = f;
-//			println(f);
-//			n--;
-//		}
-		while(true) {
-			int n = readInt("enter n: ");
-			
-			int fibN = fib(n);
-			println(fibN);
-		}
+		int n = readInt("Please, enter Fibonacci numbers bound: ");
+		
+		int fib = fibonacci(n);
+		println(fib);
+		
 	}
 	
-	private int fib(int n) {
-		if (n == 0 || n == 1) {
+	private int fibonacci(int n){
+		if(n == 0 || n == 1){
 			return n;
 		}
 		
-		int prev2 = 0;
-		int prev1 = 1;
+		int f = -1;
+		int f1 = 1;
+		int f2 = 0;
 		
-//		loop
-		int curr = -1;
-		for (int i = 2; i <= n; i++) {
-			curr = prev2 + prev1;
-			prev2 = prev1;
-			prev1 = curr;
+		while(n > 0){
+			f = f1 + f2;
+			f2 = f1;
+			f1 = f;
 		}
 		
-		return curr;
+		return f;
 	}
 }
