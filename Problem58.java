5ff7d6444126b9742bd7c372c28114c669d5be02
S <s>
2024-01-27 03:47:17 +0400

    {"types":"C","runs":16}

--- Problem58.java
+++ Problem58.java
@@ -1,4 +1,50 @@
+import acm.program.ConsoleProgram;
+//58. (2018 წლის გამოცდის ამოცანა)
+//მატრიცას ეწოდება მაგიური თუკი ის კვადრატულია(n x n-ზე) და მასში არის ყველა
+//რიცხვი 1-დან n^2-ის ჩათვლით. თქვენი მიზანია დაწეროთ magicMatrix მეთოდი
+//რომელიც შეამოწმებს არის თუ არა მატრიცა მაგიური და თუკი მაგიურია მაშინ
+//დააბრუნებს true-ს თუ არა მაშინ false-ს.
 
-public class Problem58 {
-
+public class Problem58 extends ConsoleProgram{
+	public void run(){
+		int[][] test = {
+				{1, 2, 3},
+				{5, 6, 7},
+				{8, 9, 10}};
+		
+		if(magicMatrix(test)){
+			println("true");
+		}else{
+			println("flase");
+		}
+	}
+	
+	private boolean magicMatrix(int[][] matrix){
+		int rows = matrix.length;
+		int columns = matrix[0].length;
+		
+		if(rows != columns){
+			return false;
+		}
+		
+		for(int i = 0; i < rows*columns; i++){
+			if(!containsNumber(matrix, i)){
+				return false;
+			}
+		}
+		
+		return false;
+	}
+	
+	private boolean containsNumber(int[][] matrix, int n){
+		for(int j = 0; j < matrix.length; j++){
+			for(int i = 0; i < matrix[0].length; i++){
+				if(matrix[j][i] == n){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 }
