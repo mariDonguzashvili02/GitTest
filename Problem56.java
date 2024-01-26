3c65fad35159cd20709f2d139be843cd4880e912
S <s>
2024-01-27 02:58:29 +0400

    {"types":"C","runs":14}

--- Problem56.java
+++ Problem56.java
@@ -19,16 +19,16 @@ public class Problem56 extends ConsoleProgram{
 	}
 	
 	private int[] sorter(int[] numbers){
-		int[] sorted = new int[numbers.length];
-		int j = 0;
-		for(int i = 1; i < numbers.length; i++){
-			if(numbers[j] < numbers[i]){
-				sorted[j] = numbers[j];
-				j++;
-			}
-			
-		}
-		return sorted;
+		for (int i = 0; i < numbers.length - 1; i++) {
+	        for (int j = 0; j < numbers.length - 1 - i; j++) {
+	            if (numbers[j] > numbers[j + 1]) {
+	                int temp = numbers[j];
+	                numbers[j] = numbers[j + 1];
+	                numbers[j + 1] = temp;
+	            }
+	        }
+	    }
+		return numbers;
 	}
 	
 }
