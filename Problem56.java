ebde2e48b824a42c0c6f6671ca6596640ddffc74
S <s>
2024-01-27 02:52:22 +0400

    {"types":"C","runs":7}

--- Problem56.java
+++ Problem56.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
+//56. კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, დაალაგეთ რიცხვები
+//ზრდადობით და დაბეჭდეთ.
 
-public class Problem56 {
-
+public class Problem56 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Please enter number of numbers: ");
+		int[] numbers = new int[n];
+		for(int i = 0; i < n ; i++){
+			int m = readInt("Enter number: ");
+			numbers[i] = m;
+		}
+		
+		int[] sorted = sorter(numbers);
+		
+		for(int i = 0; i < numbers.length; i++){
+			println(sorted[i]);
+		}
+	}
+	
+	private int[] sorter(int[] numbers){
+		int[] sorted = new int[numbers.length];
+		int j = 0;
+		for(int i = 1; i < numbers.length; i++){
+			if(numbers[i - 1] < numbers[i]){
+				sorted[j] = numbers[i - 1];
+			}
+			j++;
+		}
+		return sorted;
+	}
+	
 }
