7ed07685eb8665f032fc79dc4192cbefb8efa9fc
S <s>
2024-01-27 02:29:31 +0400

    {"types":"C","runs":1}

--- Problem54.java
+++ Problem54.java
@@ -1,4 +1,42 @@
+import acm.program.ConsoleProgram;
+//54. კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, იპოვეთ და დაბეჭდეთ ამ
+//რიცხვებს შორის ორი მაქსიმალური რიცხვი.
 
-public class Problem54 {
-
+public class Problem54 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Please enter number of numbers: ");
+		int[] numbers = new int[n];
+		for(int i = 0; i < n ; i++){
+			int m = readInt("Enter number: ");
+			numbers[i] = m;
+		}
+		
+		println("Max numbers are: " + findMax(numbers) + " , " + findMax(elementDeleter(numbers, findMax(numbers))));
+		
+		
+	}
+	
+	private int findMax(int[] numbers){
+		int max = numbers[0];
+		int i = 1;
+		while (i < numbers.length)
+		{
+		    if (numbers[i] > max)
+		    {
+		        max = numbers[i];
+		    }
+		    i++;
+		}
+		return max;
+	}
+	
+	private int[] elementDeleter(int[] array, int n){
+		int[] filtered = new int[array.length - 1];
+		for(int i = 0; i < array.length; i++){
+			if(array[i] != n){
+				filtered[i] = array[i];
+			}
+		}
+		return filtered;
+	}
 }
