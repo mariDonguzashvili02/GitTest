757bb2ea947995c3b5a4f069b5512a98717f65d2
S <s>
2024-01-29 19:55:16 +0400

    {"types":"C","runs":35}

--- Problem63.java
+++ Problem63.java
@@ -1,4 +1,38 @@
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
-public class Problem63 {
+import acm.program.ConsoleProgram;
+//63. კონსოლიდან შეგვყავს წყვილ-წყვილად მეგობრების სახელები, მანამ სანამ არ შევიყვანთ
+//ცარიელ სტრინგს. დაბეჭდეთ იმ ადამიანის სახელი ვისაც ჰყავს ყველაზე მეტი მეგობარი.
+//მეგობრობა შეიძლება იყოს ცალმხრივი. თუკი შევიყვანთ A-ს და B-ს, ეს ნიშნავს, რომ A არის
+//B-ს მეგობარი, მაგრამ B შეიძლება არ იყოს A-ს მეგობარი.
 
+public class Problem63 extends ConsoleProgram{
+	private static String SENTINEL = "";
+
+	public void run(){
+		HashMap<String, Integer> friends = new HashMap<String, Integer>();
+		while(true){
+			String text = readLine("Enter friends: ");
+			if(text.equals(SENTINEL)){
+				break;
+			}
+			
+			StringTokenizer tokenizer = new StringTokenizer(text, " and ");
+			String friend1 = tokenizer.nextToken();
+			if(!friends.containsKey(friend1)){
+				friends.put(friend1, 0);
+			}
+			
+			int newCount = friends.get(friend1) + 1;
+			friends.put(friend1, newCount);
+			
+		}
+		
+		println(friends);
+		
+
+	}
+	
+	
 }
